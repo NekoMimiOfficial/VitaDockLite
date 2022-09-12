d23d32847c740df998f3fa6a7366957f4ab9928f
Higor Henrique <71106509+higorhgoncalves@users.noreply.github.com>
2022-09-12 11:32:40 -0300

    Update install.sh
    Added distro verification and the possibility to install dependencies on ArchLinux based distros.

--- install.sh
+++ install.sh
@@ -1,10 +1,39 @@
-#!/bin/sh
+#!/bin/bash
 
-echo "Installing dependencies using apt-get and pip3..."
-sudo apt-get install python3 python3-pip mpv xwallpaper v4l-utils
-sudo -H pip3 install NekoMimi
+function getDistro() {
+    sh -c "cat /etc/*-release | grep ID_LIKE= | cut -f2- -d ="
+}
 
-echo "Setting VitaDockLite as an executable file..."
-chmod +x bin/VDockLite
+distro=$(getDistro)
 
-echo "All done! enjoy VitaDockLite <3"
+echo "Verifying Linux Distribution and trying to install dependencies..."
+if [ $distro = "debian" ] 
+then 
+    sudo apt update
+    sudo apt install -y python3 python3-pip mpv xwallpaper v4l-utils
+    
+    #define variable to continue dependencies installation
+    continue=1
+elif [ $distro = "arch" ] 
+then 
+    sudo pacman -Syu --noconfirm python python-pip mpv xwallpaper v4l-utils
+    
+    #define variable to continue dependencies installation
+    continue=1
+else 
+    echo "Linux Distribution not supported"
+    
+    #define variable to cancell dependencies installation
+    continue=0
+fi
+
+
+if [ $continue -eq 1 ]
+then
+    sudo -H pip3 install NekoMimi
+
+    echo "Setting VitaDockLite as an executable file..."
+    chmod +x bin/VDockLite
+
+    echo "All done! enjoy VitaDockLite <3"
+fi
