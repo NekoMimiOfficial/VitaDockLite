c6ad2c9f9740d9d782825c1139dc5c3915731330
NekoMimi <nekomimi@tilde.team>
2023-01-11 22:15:29 +0300

    fixed install script

--- install.sh
+++ install.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 
-function getDistro() {
-    sh -c "cat /etc/*-release | grep ID_LIKE= | cut -f2- -d ="
-}
-
-distro=$(getDistro)
-
+#distro=$(getDistro)
+distroID=$(cat /etc/*-release | grep ^ID=)
+distro=$(echo $distroID | cut -d '=' -f2)
+echo $distro
 echo "Verifying Linux Distribution and trying to install dependencies..."
-if ( $distro == "debian" ) 
+if [ $distro == "debian" ] 
 then 
     sudo apt update
     sudo apt install -y python3 python3-pip mpv xwallpaper v4l-utils
     
     #define variable to continue dependencies installation
     continue=1
-elif [ $distro = "arch" ] 
+elif [ $distro == "arch" ] 
 then 
     sudo pacman -Syu --noconfirm python python-pip mpv xwallpaper v4l-utils
     
